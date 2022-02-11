a0d40de22f3f6488de74b707721fe8f1fbbe4721
Lane Garner <b.lane.garner@gmail.com>
2022-02-10 22:03:09 -0600

    add edit support for more params

--- src/screens/timer/components/Recipe.jsx
+++ src/screens/timer/components/Recipe.jsx
@@ -6,12 +6,7 @@ import { Context } from "../../../Context";
 import { TimerTitle, TimerValue, TimerValueEdit } from "../../../utils/styled";
 import { TouchableTimerContainer, TimerContainer, TimerContainerAlt, Btn, BtnText } from "../styled";
 
-export const Recipe = ({ next }) => {
-  const { ratio, setRatio, coffee, setCoffee, brewMethod, setBrewMethod, recipe, setRecipe, active, setActive } = useContext(Context);
-  const [editCoffee, setEditCoffee] = useState(false);
-  const [coffeeState, setCoffeeState] = useState(Math.round(recipe.coffee).toString());
-
-  const methods = [
+const methods = [
     {
       method: "chemex",
       ratio: 15.5,
@@ -66,18 +61,49 @@ export const Recipe = ({ next }) => {
     },
   ];
 
-  const handleEditCoffeeClick = () => setEditCoffee(!editCoffee);
+export const Recipe = ({ next }) => {
+  const { brewMethod, recipe, setRecipe } = useContext(Context);
+  
+  const [coffeeState, setCoffeeState] = useState(Math.round(recipe.coffee).toString());
+  const [editCoffee, setEditCoffee] = useState(false);
+  
+  const [ratioState, setRatioState] = useState(recipe.ratio.toString());
+  const [editRatio, setEditRatio] = useState(false);
+  
+  const [tempState, setTempState] = useState(recipe.temp.toString());
+  const [editTemp, setEditTemp] = useState(false);
+
+  const [bloomState, setBloomState] = useState(recipe.bloom.toString());
+  const [editBloom, setEditBloom] = useState(false);
+  
+  const [intervalState, setIntervalState] = useState(recipe.interval.toString());
+  const [editInterval, setEditInterval] = useState(false);
+
+  const handleEditCoffee = () => setEditCoffee(!editCoffee);
+  const handleEditRatio = () => setEditRatio(!editRatio);
+  const handleEditTemp = () => setEditTemp(!editTemp);
+  const handleEditBloom = () => setEditBloom(!editBloom);
+  const handleEditInterval = () => setEditInterval(!editInterval);
 
   const handleDismissEdit = () => {
-    if(editCoffee) {
+    if (editCoffee) {
       setEditCoffee(false)
+    } else if (editRatio) {
+      setEditRatio(false)
+    } else if (editTemp) {
+      setEditTemp(false)
+    } else if (editBloom) {
+      setEditBloom(false)
+    } else if (editInterval) {
+      setEditInterval(false)
     }
   }
 
   useEffect(() => {
-    if (coffeeState === "") return;
-    setRecipe({ ...recipe, coffee: parseInt(coffeeState) });
-  }, [coffeeState]);
+    if (coffeeState === "" || ratioState === "" || tempState === "" || bloomState === "" || intervalState === "") return;
+    // TODO: local storage
+    setRecipe({ ...recipe, coffee: parseInt(coffeeState), ratio: parseFloat(ratioState), temp: parseInt(tempState), bloom: parseInt(bloomState), interval: parseInt(intervalState) });
+  }, [coffeeState, ratioState, tempState, bloomState, intervalState]);
 
   useEffect(() => methods.forEach((item) => item.method === brewMethod && setRecipe(item)), [brewMethod]);
 
@@ -87,17 +113,25 @@ export const Recipe = ({ next }) => {
         <TimerTitle>brew method</TimerTitle>
         <TimerValue>{brewMethod}</TimerValue>
       </TimerContainer>
-      <TimerContainer>
+      <TouchableTimerContainer onPress={handleEditRatio}>
         <TimerTitle>ratio</TimerTitle>
-        <TimerValue>{recipe.ratio.toFixed(1)}:1</TimerValue>
-      </TimerContainer>
+        {editRatio ? (
+          <View style={{ flexDirection: "row" }}>
+            <TimerValueEdit onChangeText={setRatioState} value={ratioState} keyboardType="decimal-pad" keyboardAppearance="dark" autoFocus />
+            <TimerValue>:1</TimerValue>
+          </View>
+          ) : (
+          <TimerValue>{recipe.ratio % 1 === 0 ? recipe.ratio : recipe.ratio.toFixed(1)}:1</TimerValue>
+          )
+        }
+      </TouchableTimerContainer>
       <TimerContainerAlt>
         <TimerContainer>
           <TimerTitle>water</TimerTitle>
-          <TimerValue>{Math.round(recipe?.coffee * ratio * 100) / 100 || Math.round(recipe?.tea * ratio * 100) / 100}g</TimerValue>
+          <TimerValue>{Math.round(recipe?.coffee * recipe.ratio * 100) / 100 || Math.round(recipe?.tea * recipe.ratio * 100) / 100}g</TimerValue>
         </TimerContainer>
         {recipe.coffee && (
-          <TouchableTimerContainer onPress={handleEditCoffeeClick}>
+          <TouchableTimerContainer onPress={handleEditCoffee}>
             <TimerTitle>coffee</TimerTitle>
             {editCoffee ? (
               <View style={{ flexDirection: "row" }}>
@@ -122,20 +156,44 @@ export const Recipe = ({ next }) => {
           <TimerValue>{recipe.grind}</TimerValue>
         </TimerContainer>
       )}
-      <TimerContainer>
+      <TouchableTimerContainer onPress={handleEditTemp}>
         <TimerTitle>temp</TimerTitle>
-        <TimerValue>{recipe.temp}&#176; f</TimerValue>
-      </TimerContainer>
-
+        {editTemp ? (
+          <View style={{ flexDirection: "row" }}>
+            <TimerValueEdit onChangeText={setTempState} value={tempState} keyboardType="decimal-pad" keyboardAppearance="dark" autoFocus />
+            <TimerValue>&#176; f</TimerValue>
+          </View>
+          ) : (
+          <TimerValue>{recipe.temp}&#176; f</TimerValue>
+          )
+        }
+      </TouchableTimerContainer>
       <TimerContainerAlt>
-        <TimerContainer>
+        <TouchableTimerContainer onPress={handleEditBloom}>
           <TimerTitle>bloom</TimerTitle>
-          <TimerValue>{recipe.bloom}s</TimerValue>
-        </TimerContainer>
-        <TimerContainer>
+            {editBloom ? (
+            <View style={{ flexDirection: "row" }}>
+              <TimerValueEdit onChangeText={setBloomState} value={bloomState} keyboardType="decimal-pad" keyboardAppearance="dark" autoFocus />
+              <TimerValue>s</TimerValue>
+            </View>
+            ) : (
+            <TimerValue>{recipe.bloom}s</TimerValue>
+            )
+          }
+
+        </TouchableTimerContainer>
+        <TouchableTimerContainer onPress={handleEditInterval}>
           <TimerTitle>interval</TimerTitle>
-          <TimerValue>{recipe.interval}s</TimerValue>
-        </TimerContainer>
+          {editInterval ? (
+            <View style={{ flexDirection: "row" }}>
+              <TimerValueEdit onChangeText={setIntervalState} value={intervalState} keyboardType="decimal-pad" keyboardAppearance="dark" autoFocus />
+              <TimerValue>s</TimerValue>
+            </View>
+            ) : (
+            <TimerValue>{recipe.interval}s</TimerValue>
+            )
+          }
+        </TouchableTimerContainer>
       </TimerContainerAlt>
       <Btn style={{ marginTop: 25 }} onPress={next}>
         <BtnText>start</BtnText>
